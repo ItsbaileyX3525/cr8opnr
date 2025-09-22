cf184373b23257ff0d9a4799595c8ddecdf8a773
Bailey Miles <80989969+ItsbaileyX3525@users.noreply.github.com>
2025-09-22 16:00:03 +0100

    Add some cool little things

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-;
+const profileContainer = document.getElementById("profile-container") as HTMLImageElement;
+const gemBalance = document.getElementById("gem-balance") as HTMLParagraphElement;
+const profileContainerDropDown = document.getElementById("profile-dropdown") as HTMLDivElement;
+
+profileContainer.addEventListener("click", () => {
+    if (profileContainerDropDown.classList.contains("hidden")) {
+        profileContainerDropDown.classList.remove("hidden")
+    } else {
+        profileContainerDropDown.classList.add("hidden")
+    }
+});
+
+(()=>{(()=>{(()=>{console.log("This works lol")})();})();})();
