860c0995cc6f76dc37aed4a16f8a33766decb349
Bailey Miles <baileyleah542@gmail.com>
2025-09-24 00:49:23 +0100

    Doesnt really matter but makes it look cleaner

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const pool = mariadb.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     connectionLimit: 5,
-    database: "cr8opnrUsers"
+    database: process.env.DB_NAME,
 });
 const saltRounds = 10;
 
